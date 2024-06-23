cef857c86bc62b292e80ed655c038cd3f786259c
Adonis2030 <service.provider2030@gmail.com>
2024-06-23 13:36:11 +0200

    add express server and routes for user, products

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const http = require("http");
+require("dotenv").config();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
+const express = require("express");
+const app = express();
 
-  res.write("<h1>Hello, Node.js HTTP Server!</h1>");
-  res.end();
+app.get("/", (req, res) => {
+  res.send("hello root node");
+  //   this gets executed when you visit http://localhost:3000/
 });
 
-const port = 8000;
+// Include route files
+const usersRoute = require("./routes/users");
+const productsRoute = require("./routes/products");
 
-server.listen(port, () => [
-  console.log(`Node.js HTTP server is running on port ${port}`),
-]);
+// Use routes
+app.use("/users", usersRoute);
+app.use("/products", productsRoute);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
